b92227fb0079549d7ebd1cde2e1364a50610c33f
JackFirth <jackhfirth@gmail.com>
2014-11-13 15:39:14 -0800

    Examples
    Split examples into separate module

---
+++ examples.rkt
@@ -0,0 +1,31 @@
+#lang typed/racket
+
+(require "haskell-type-arrow.rkt")
+
+(:: f A B => (A -> B) (Listof A) -> (Listof B))
+(define (f mf as)
+  (map mf as))
+
+(:: $ A B => A -> (A -> B) -> B)
+(define (($ a) f)
+  (f a))
+
+(:: compare-project A B => (B B -> Boolean) -> (A -> B) -> A A -> Boolean)
+(define (((compare-project cmp) a->b) v1 v2)
+  (cmp (a->b v1)
+       (a->b v2)))
+
+(:: compare-as-real A => (A -> Real) -> A A -> Boolean)
+(define compare-as-real ((inst compare-project Real) <))
+
+(:: string-shorter? String String -> Boolean)
+(define string-shorter?
+  (((inst compare-project Real) <) string-length))
+
+(:: zipWith A B C => (A B -> C) -> (Listof A) (Listof B) -> (Listof C))
+(define ((zipWith zipper) as bs)
+  (map zipper as bs))
+
+(:: map-list-to-vector A B => (Listof A) -> (A -> B) -> (Vectorof B))
+(define ((map-list-to-vector lst) f)
+  (list->vector (map f lst)))
